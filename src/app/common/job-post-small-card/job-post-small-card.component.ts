9eb704ac357a10615f69997c1fe641c4443eca17
naruka1023 <naruka1023@yahoo.com>
2023-01-05 16:35:00 +0700

    minor fixes

--- src/app/common/job-post-small-card/job-post-small-card.component.ts
+++ src/app/common/job-post-small-card/job-post-small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-job-post-small-card',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./job-post-small-card.component.css']
 })
 export class JobPostSmallCardComponent {
-
+  @Input() fullTimeFlag = true 
 }
