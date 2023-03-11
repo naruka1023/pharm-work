6a6b95ab225971b398fd6bbb48f746d96b54c7b3
naruka1023 <naruka1023@yahoo.com>
2023-03-11 10:39:40 +0700

    moar commits

--- src/app/operator/page/operator-profile/operator-profile.component.ts
+++ src/app/operator/page/operator-profile/operator-profile.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Subscription, take } from 'rxjs';
 import { JobService } from '../../service/job.service';
-import { ProfileService } from '../../service/profile.service';
+import { UtilService } from '../../service/util.service';
 import { getCreatedJobsSuccess } from '../../state/actions/job-post.actions';
 declare var window: any;
 
@@ -11,24 +11,32 @@ declare var window: any;
   templateUrl: './operator-profile.component.html',
   styleUrls: ['./operator-profile.component.css']
 })
-export class OperatorProfileComponent {
+export class OperatorProfileComponent implements OnDestroy{
   subscription:Subscription = new Subscription;
   formModal!: any
   loadingFlag: boolean = true;
 
-  constructor(private profileService:ProfileService, private store:Store, private jobService:JobService){}
+  constructor(private profileService:UtilService, private store:Store, private jobService:JobService){}
   ngOnInit(): void {
     this.subscription.add(this.store.select((state:any)=>{
       return state.createdJobs.loading
     }).subscribe((loading)=>{
       this.loadingFlag = loading;
     }))
+    this.subscription.add(
+      this.store.select((state:any)=>{
+        return state.operatorProfile.url !== ''? state.operatorProfile.url : '';
+      }).pipe(take(1)).subscribe((url: string)=>{
+        console.log('print')
+        document.getElementById(url)?.click();
+      })
+    )
     this.subscription.add(this.profileService.getRevertTabSubject().subscribe(()=>{
-      document.getElementById('profileTab')?.click();
+      document.getElementById('inner-profile')?.click();
     }));
     this.subscription.add(this.store.select((state: any)=>{
       return state.user.uid
-    }).subscribe((operatorUID:any)=>{
+    }).subscribe((operatorUID:any) =>{
       if(operatorUID !== '' && this.loadingFlag){
         this.jobService.getJobsCreated(operatorUID).subscribe((jobs)=>{
           this.store.dispatch(getCreatedJobsSuccess({jobs:jobs}));
